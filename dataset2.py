1f66039537afa320fde3fee63d61384a0b8b6eda
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2024-05-21 17:18:57 +0530

    Add files via upload
    Signed-off-by: Aditya  <119723334+22MH1A42G1@users.noreply.github.com>

---
+++ dataset2.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 31 15:37:30 2023
+
+@author: LENOVO
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 31 14:13:17 2023
+//find Candidate Elimination algorithm
+@author: LENOVO
+"""
+import pandas as  pd
+d=pd.DataFrame([['Sunny','Warm','Normal','Strong','Warm','Same','Yes'],
+                ['Sunny','Warm','high','Strong','Warm','Same','Yes'],
+                ['rainy','cold','high','Strong','Warm','change','no'],
+                ['Sunny','Warm','high','Strong','cool','change','no']])
+a = list(d.iloc[:, :-1].values)
+b = list(d.iloc[:, -1].values)
+s = ['@' for i in range(len(a[0]))]
+g = [['?' for i in range(len(a[0]))]
+     for j in range(len(a[0]))]
+r = 0
+for i in range(len(b)):
+    if b[i] == 'Yes' and r == 0:
+        s = a[i].copy()
+        r += 1
+    if b[i] == 'Yes' and r>0:
+        for j in range(len(s)):
+            if s[j] != a[i][j]:
+                s[j] = '?'
+    if b[i] == 'no':
+        for k in range(len(g)):
+            if s[k] != a[i][k]:
+                g[k][k]=s[k]
+            else:
+                g[k][k] = '?'
+print(s)
+print(g)
+
+n = []
+for i in range(len(s)):
+    if s[i] != g[i][i]:
+        g[i][i]= '?'
+    if g[i].count('?')!=len(s):
+        n.append(g[i])
+print(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
